47c6ad2b6b1524760cd11b16ed1ed34f9dc882d2
fruitfallbot2 <fruitfallbot2@gmail.com>
2024-08-18 15:28:07 +0530

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
-
 	public static void main(String[] args) {
 		System.out.println("i am first commit");
+		System.out.println("i am second commit. but first from eclipse IDE");
 	}
 }
